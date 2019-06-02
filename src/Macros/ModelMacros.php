0b28bc7bc604c2901be62f7d2da50b0228a7399b
Hitesh Kumar <hitesh.kumar399@gmail.com>
2019-06-02 12:24:13 +0530

    fixed Model macros issue

--- src/Macros/ModelMacros.php
+++ src/Macros/ModelMacros.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Builder;
 
 class ModelMacros {
-	
+
 	protected $builder;
 	protected $request;
 
@@ -15,24 +15,24 @@ class ModelMacros {
 		$this->request = $request;
 	}
 
-  protected function dbd($array) {
-		$new_q = '';
-		$query = $array['query'];
-		$bindings = $array['bindings'];
-		$limit = strlen($query);
-		$occurences = 0;
-		$temp_string = '';
-		for ($i = 0; $i < $limit; $i++) {
-			if($query[$i] == '?') {
-				$temp_string .= "'" . $bindings[$occurences]. "'";
-				$occurences++;
+    protected function dbd($array) {
+        $new_q = '';
+        $query = $array['query'];
+        $bindings = $array['bindings'];
+        $limit = strlen($query);
+        $occurences = 0;
+        $temp_string = '';
+        for ($i = 0; $i < $limit; $i++) {
+            if($query[$i] == '?') {
+                $temp_string .= "'" . $bindings[$occurences]. "'";
+                $occurences++;
 
-			} else {
-				$temp_string .= $query[$i];
-			}
-		}
-		$new_q = $temp_string;
-		return $new_q;
+            } else {
+                $temp_string .= $query[$i];
+            }
+        }
+        $new_q = $temp_string;
+        return $new_q;
 	}
 
 	public function getSql() {
@@ -52,12 +52,16 @@ class ModelMacros {
 
 	public function lqPaginate($columns = ['*']) {
 		$page = $this->request->page ? $this->request->page : 1;
-		$perPage = $this->request->page_size ? $this->request->page_size : 15;
-		$results = $this->builder->forPage($page, $perPage)->get($columns);
+        $perPage = $this->request->page_size ? $this->request->page_size : 4;
+        $builder = clone $this->builder;
+        if ($perPage != '-1') {
+            $builder->forPage($page, $perPage);
+        }
+        $results = $builder->get($columns);
 		$data = [];
 		$data['data'] = $results;
 		if ($page === 1) {
-			$data['total'] = $this->total();
+			$data['total'] = $perPage == '-1' ? $results->count() : $this->total();
 		}
 		return $data;
 	}
