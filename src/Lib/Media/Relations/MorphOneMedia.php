ed032ffa7a8e66f1e6e2d0fc21495ea4d0114bc5
Hitesh Kumar <hitesh.kumar399@gmail.com>
2019-06-04 00:39:51 +0530

    update

--- src/Lib/Media/Relations/MorphOneMedia.php
+++ src/Lib/Media/Relations/MorphOneMedia.php
@@ -11,26 +11,22 @@ class MorphOneMedia extends MorphOne {
      * Store the Media File and update in relation.
      */
     public function addMedia(Array $file = null, $path = null, $thumbnails = null) {
-
-        if ($file) {
-            $media = null;
-            if (isset($file['file']) && !empty($file['file'])) {
-                $uploader = new MediaUploader($file, $path, $thumbnails);
-                $data = $uploader->uploadAndPrepareData();
-                $relation_array = $this->make()->toArray();
-                $media = $this->updateOrCreate($relation_array, $data);
-            } else if (isset($file['id']) && !empty($file['id'])) {
-                $media = clone $this->getQuery();
-                $media = $media->where('id', $file['id'])->first();
-            }
-            if($this->parent->mediaMorphRelation) {
-                $this->parent->setRelation($this->parent->mediaMorphRelation, $media);
-            }
-
+        $media = null;
+        if (isset($file['file']) && !empty($file['file'])) {
+            $uploader = new MediaUploader($file, $path, $thumbnails);
+            $data = $uploader->uploadAndPrepareData();
+            $relation_array = $this->make()->toArray();
+            $media = $this->updateOrCreate($relation_array, $data);
+        } else if (isset($file['id']) && !empty($file['id'])) {
+            $media = clone $this->getQuery();
+            $media = $media->where('id', $file['id'])->first();
         } else {
             $this->unlinkRelation();
-            $this->parent->setRelation($this->parent->mediaMorphRelation, null);
         }
+        if ($this->parent->mediaMorphRelation) {
+            $this->parent->setRelation($this->parent->mediaMorphRelation, $media);
+        }
+        return $this;
     }
 
     protected function unlinkRelation() {
