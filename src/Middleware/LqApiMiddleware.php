5c30c3f1a0f4281d85f638f7f863b216c84c913c
Hitesh Kumar <hitesh.kumar399@gmail.com>
2019-10-16 00:11:35 +0530

    fixed middleware issue

--- src/Middleware/LqApiMiddleware.php
+++ src/Middleware/LqApiMiddleware.php
@@ -15,20 +15,24 @@ class LqApiMiddleware extends Authenticate
     /**
      * Handle an incoming request.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param \Closure                 $next
-     * @param string|null              $guard
+     * @param \Illuminate\Http\Request $request [All data]
+     * @param \Closure                 $next    [Callback function to execute next process]
+     * @param string|null              $guard   [Auth Guard]
      *
      * @return mixed
      */
     public function handle($request, Closure $next, ...$guard)
     {
-        $request::macro('client', function () {
-            return null;
-        });
-        $request::macro('device', function () {
-            return null;
-        });
+        $request::macro(
+            'client', function () {
+                return null;
+            }
+        );
+        $request::macro(
+            'device', function () {
+                return null;
+            }
+        );
 
         // Save Request Error Log into Database
         if (env('LQ_WRITE_ERROR')) {
@@ -38,16 +42,16 @@ class LqApiMiddleware extends Authenticate
         /*
          * Client ID should be present in every request header
          */
-        $this->verifyClient($request);
+        $this->_verifyClient($request);
 
         /*
          * the device id should be present in every request header
          */
-        $this->findDeviceInfo($request);
+        $this->_findDeviceInfo($request);
         /*
          * Check the Route Access
          */
-        $this->checkRouteAccess($request, ['api']);
+        $this->_checkRouteAccess($request, ['api']);
 
         $response = $next($request);
         if (env('LQ_WRITE_ERROR')) {
@@ -59,8 +63,10 @@ class LqApiMiddleware extends Authenticate
 
     /**
      * throw exception, if Client id is not valid.
+     *
+     * @return void|
      */
-    private function invalidClientResponse()
+    private function _invalidClientResponse()
     {
         app('Lq\Response')->message = trans('auth.invalid_client');
         throw ValidationException::withMessages([]);
@@ -68,8 +74,10 @@ class LqApiMiddleware extends Authenticate
 
     /**
      * Throw the exception, if Device is not present in header.
+     *
+     * @return void|
      */
-    private function invalidDeviceIdResponse()
+    private function _invalidDeviceIdResponse()
     {
         app('Lq\Response')->message = trans('auth.invalid_device_id');
         throw ValidationException::withMessages([]);
@@ -77,8 +85,12 @@ class LqApiMiddleware extends Authenticate
 
     /**
      * To verify the client id.
+     *
+     * @param \Illuminate\http\Request $request [All Request data]
+     *
+     * @return void|
      */
-    private function verifyClient(Request $request)
+    private function _verifyClient(Request $request)
     {
         $lq_client = new ClientRepository();
         try {
@@ -86,43 +98,73 @@ class LqApiMiddleware extends Authenticate
             $client = $lq_client->getClient($client_id);
 
             if (!$client) {
-                $this->invalidClientResponse();
+                $this->_invalidClientResponse();
             }
-            $request::macro('client', function () use ($client) {
-                return $client;
-            });
+            $request::macro(
+                'client', function () use ($client) {
+                    return $client;
+                }
+            );
         } catch (DecryptException $e) {
-            $this->invalidClientResponse();
+            $this->_invalidClientResponse();
         }
     }
 
     /**
      * To Find and create the device.
+     *
+     * @param \Illuminate\http\Request $request [All Request data]
+     *
+     * @return void|
      */
-    private function findDeviceInfo(Request $request)
+    private function _findDeviceInfo(Request $request)
     {
         if (!$request->header('device-id')) {
-            $this->invalidDeviceIdResponse();
+            $this->_invalidDeviceIdResponse();
         }
 
         $device_id = $request->header('device-id');
-        $request::macro('device', function () use ($device_id, $request) {
-            $model = config('lq.device_class');
+        $request::macro(
+            'device', function () use ($device_id, $request) {
+                $model = config('lq.device_class');
 
-            return $model::firstOrCreate(['device_id' => $device_id], ['device_id' => $device_id, 'client_id' => $request->client()->id]);
-        });
+                return $model::firstOrCreate(
+                    ['device_id' => $device_id],
+                    [
+                        'device_id' => $device_id,
+                        'client_id' => $request->client()->id,
+                    ]
+                );
+            }
+        );
     }
 
-    private function _authenticateWithException(Request $request)
+    /**
+     * Authenticate user without throw the Exception.
+     *
+     * @param \Illuminate\http\Request $request [All Request data]
+     * @param array                    $guards  [Auth gaurd]
+     *
+     * @return void|
+     */
+    private function _authenticateWithoutException(Request $request, $guards)
     {
         if ($request->header('Authorization')) {
             try {
                 $this->authenticate($request, $guards);
             } catch (\Exception $e) {
+                // No need to throw exception,
             }
         }
     }
 
+    /**
+     * To set the current role in Permission Repo.
+     *
+     * @param \Illuminate\http\Request $request [All Request data]
+     *
+     * @return void|
+     */
     private function _setCurrentRoleIdsInPermissonRepo(Request $request): void
     {
         $permission = app('permission');
@@ -130,7 +172,7 @@ class LqApiMiddleware extends Authenticate
         $user_device = \Auth::user()->devices()->where(
             'devices.id', $request->device()->id
         )->first();
-
+        // dd($user_device);
         if ($user_device) {
             if ($request->client()->role_access_type == 'one_at_time') {
                 $permission->setCurrentRoleIds([$user_device->pivot->role_id]);
@@ -142,11 +184,18 @@ class LqApiMiddleware extends Authenticate
         }
     }
 
+    /**
+     * To add only current app role in Auth Collection.
+     *
+     * @param \Illuminate\http\Request $request [All Request data]
+     *
+     * @return void|
+     */
     private function _onlyAppRoles(Request $request): void
     {
         $client_id = $request->client()->id;
         $roles = $request->user()->roles->filter(
-            function ($item, $key) use ($client_id) {
+            function ($item) use ($client_id) {
                 return in_array($client_id, $item->client_ids);
             }
         );
@@ -155,42 +204,62 @@ class LqApiMiddleware extends Authenticate
 
     /**
      * To check the current route permission for the login user.
+     *
+     * @param \Illuminate\http\Request $request [All Request data]
+     *
+     * @return void|
      */
-    private function checkRouteAccess($request, $guards)
+    private function _checkRouteAccess($request, $guards)
     {
         $permission = app('permission');
+
         /**
          * Get the Current Route detail from database or cache.
          */
         $current_permission = $permission->current();
-
-        $lq_response = app('Lq\Response');
 
         if ($current_permission && isset($current_permission['is_public']) && $current_permission['is_public'] == 'N') {
             // Check the user does have the valid token to access current route.
             if (\Config::get('lq.check_authentication')) {
                 $this->authenticate($request, $guards);
             } else {
-                $this->_authenticateWithException($request);
+                $this->_authenticateWithoutException($request, $guards);
             }
 
-            if ($request->user()) {
-                // Get Login user Role id.
+            $this->_assignCurrentPermissions($request);
+        } elseif ($request->header('Authorization')) {
+            $this->_authenticateWithoutException($request, $guards);
+            $this->_assignCurrentPermissions($request);
+        }
+    }
 
-                $this->_onlyAppRoles($request);
-                $this->_setCurrentRoleIdsInPermissonRepo($request);
-                $active_role_id = $permission->canAccess();
-                if ($active_role_id) {
-                    $permission->setActiveRoleId($active_role_id);
-                }
+    /**
+     * To set the current route permission in response.
+     *
+     * @param \Illuminate\http\Request $request [All Request data]
+     *
+     * @return void|
+     */
+    private function _assignCurrentPermissions($request)
+    {
+        $permission = app('permission');
+        if ($request->user()) {
+            $lq_response = app('Lq\Response');
+            // Get Login user Role id.
+            $this->_onlyAppRoles($request);
+            $this->_setCurrentRoleIdsInPermissonRepo($request);
+            $active_role_id = $permission->canAccess();
+            if ($active_role_id) {
+                $permission->setActiveRoleId($active_role_id);
+            }
 
-                // Add Current Permission Limitation and allowed field in final response.
-                $current_permission = $permission->getCurrentRolePermissions();
+            // Add Current Permission Limitation and allowed field in final response.
+            $current_permission = $permission->getCurrentRolePermissions();
 
-                // Removing database table column from response.
-                if ($current_permission && !empty($current_permission['fields'])) {
-                    $fields = $current_permission['fields'];
-                    array_walk(
+            // Removing database table column from response.
+            if ($current_permission && !empty($current_permission['fields'])) {
+                $fields = $current_permission['fields'];
+                array_walk(
                         $fields,
                         function (&$field) {
                             unset($field['table_columns']);
@@ -198,18 +267,15 @@ class LqApiMiddleware extends Authenticate
                             return $field;
                         }
                     );
-                    $current_permission['fields'] = $fields;
-                }
-
-                $lq_response->current_permission = $current_permission;
-                // Check the curent user has the privilege to access the current route.
-
-                if (!$active_role_id && \Config::get('lq.check_authentication')) {
-                    throw new AuthorizationException();
-                }
+                $current_permission['fields'] = $fields;
             }
-        } elseif ($request->header('Authorization')) {
-            $this->_authenticateWithException($request);
+
+            $lq_response->current_permission = $current_permission;
+            // Check the curent user has the privilege to access the current route.
+
+            if (!$active_role_id && \Config::get('lq.check_authentication')) {
+                throw new AuthorizationException();
+            }
         }
     }
 }
